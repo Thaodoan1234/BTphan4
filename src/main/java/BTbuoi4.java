dd474f5f40c8a81b8bd803b5c246d3659a7e1edc
Thaodoan1234 <thao.doan1@onemount.com>
2021-08-10 21:20:58 +0700

    Initial commit

---
+++ src/main/java/BTbuoi4.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class BTbuoi4 {
+    public static void main(String[] args){
+
+        int n, temp, rem, reverse = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1 number: ");
+        n = sc.nextInt();
+
+ //       temp = n;
+ //       while(temp != 0){
+ //           rem = temp % 10;
+ //           reverse = reverse * 10 +rem;
+ //           temp /= 10;
+ //       }
+ //       if(reverse == n) {
+ //           System.out.println("This is a symmetric number");
+ //       }
+ //       else {
+ //           System.out.println("This is not a symmetric number");
+ //       }
+
+
+ //       int i = 0;
+ //       while (i*i <= n) {
+ //           if(i*i == n){
+ //               System.out.println("This is a perfect square: " + n);
+ //           }
+ //           i++;
+ //       }
+ //       System.out.println("This is not a perfect square: " + n);
+
+
+ //       int i, s=1;
+ //       for(i=1;i<= n;i++ ){
+ //           s = s*i;
+ //       }
+ //       System.out.println("Total = " + s);
+
+
+ //       int i, j,s=1, Sum=0;
+ //       for(j=1; j<=n; j++){
+ //           for(i=1; i<=n; i++){
+ //               s = s*i;
+ //           }
+ //           Sum = Sum + s;
+ //       }
+ //       System.out.println("Sum = " + Sum);
+
+
+ //        int a, b , sum = 0;
+ //       System.out.println("a,b < n");
+ //       System.out.println("Enter a: ");
+ //       a = sc.nextInt();
+ //       System.out.println("Enter b: ");
+ //       b = sc.nextInt();
+ //
+ //              if(a <n && b <n) {
+ //           for (int i=1; i<n; i++){
+ //               if(i%a == 0 && i%b != 0) {
+ //                   sum = sum + i;
+ //               }
+ //           }
+ //           System.out.println("Total: "+ sum);
+ //       }
+
+
+ //       int i, j, uoc=0, sum=0;
+ //       while (n<=0 || n>=50); {
+ //           for(i=2; i<n; i++) {
+ //               for(j=1; j<=i; j++) {
+ //                   if(i%j == 0) {
+ //                       uoc++;
+ //
+ //                   if (uoc == 2) {
+ //                       sum = sum + i;
+ //                   }
+ //                   }
+ //               }
+ //           }
+ //           System.out.println("Total = " + sum);
+ //       }
+
+
+
+
+
+    }
+}
